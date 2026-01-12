b9e1df6f44262c211be00b0c60d0ababbf7048c3
the-darby <drew.darby.md@gmail.com>
2026-01-12 12:43:29 -0500

    Integrate game clock to control FPS

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 
+	clock = pygame.time.Clock()
+	dt = 0
+
 	while True:
 		log_state()
 
@@ -19,6 +22,8 @@ def main():
 		screen.fill("black")
 		
 		pygame.display.flip()
+
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
