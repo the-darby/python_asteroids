679f7c58ffb3afd764f105a09de110ba2c4623a8
the-darby <drew.darby.md@gmail.com>
2026-01-14 17:47:49 -0500

    add collision for shots and asteroids

--- main.py
+++ main.py
@@ -48,6 +48,13 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+                
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collide_circle(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
 
         screen.fill("black")
 
