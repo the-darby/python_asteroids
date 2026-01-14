c00807ae6ba8fc94c59afcab1e6188fcc3154f28
the-darby <drew.darby.md@gmail.com>
2026-01-14 16:46:29 -0500

    add shooting speed limiter

--- player.py
+++ player.py
@@ -1,5 +1,12 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SPEED
+from constants import (
+	PLAYER_RADIUS, 
+	LINE_WIDTH, 
+	PLAYER_TURN_SPEED, 
+	PLAYER_SHOOT_SPEED, 
+	PLAYER_SPEED, 
+	PLAYER_SHOOT_COOLDOWN_SECONDS
+)
 from circleshape import CircleShape
 from shot import Shot
 
@@ -8,6 +15,7 @@ class Player(CircleShape):
 		super().__init__( x, y, PLAYER_RADIUS)
 
 		self.rotation = 0
+		self.shoot_cooldown = 0
 
 	
 	def triangle(self):
@@ -27,6 +35,7 @@ class Player(CircleShape):
 
 	def update(self, dt):
 		keys = pygame.key.get_pressed()
+		self.shoot_cooldown = max(0, self.shoot_cooldown - dt)
 
 		if keys[pygame.K_a]:
 			self.rotate(-dt)
@@ -50,6 +59,10 @@ class Player(CircleShape):
 		self.position += rotated_with_speed_vector
 
 	def shoot(self):
+		if self.shoot_cooldown > 0:
+			return
+		else:
+			self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 		shot = Shot(self.position.x, self.position.y)
 		forward = pygame.Vector2(0, 1).rotate(self.rotation)
 		shot.velocity = forward * PLAYER_SHOOT_SPEED
